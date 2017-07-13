31856b6d962efd88e8690680ce24006830f46b4f
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2017-07-12 19:48:34 -0700

    Updates

---
+++ docs/analysis_gui.R
@@ -0,0 +1,10 @@
+## ---- echo = FALSE,hide=TRUE, message=FALSE,warning = FALSE--------------
+devtools::load_all(".")
+
+## ----gui, eval=FALSE, message=FALSE,warning=F----------------------------
+#  library(TCGAbiolinksGUI)
+#  TCGAbiolinksGUI()
+
+## ----sessioninfo, eval=TRUE----------------------------------------------
+sessionInfo()
+
